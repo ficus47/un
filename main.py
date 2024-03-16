c794d30ea17cf9d647d6295308ba51b7293bcd30
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-03-16 14:37:49 +0000

    trop

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.05)
 train_generator = train_datagen.flow_from_directory(
     'NULL',
     target_size=(150, 150),
-    batch_size=64,
+    batch_size=256,
     class_mode='sparse',  # Utilisez 'sparse' pour des étiquettes numériques
     shuffle=True,
     subset='training',  # Utiliser uniquement un sous-ensemble des données d'entraînement
@@ -42,8 +42,6 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(128, (3, 3), activation='relu', input_shape=(150, 150, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 
-model.add(layers.Conv2D(128, (3, 3), activation='relu', input_shape=(150, 150, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
 
 model.add(layers.Flatten())
 model.add(layers.Dense(128, activation='relu'))
@@ -53,7 +51,7 @@ model.add(layers.Dense(1))
 
 model.compile(optimizer="adam", loss="mse", metrics=["mae", "accuracy"])
 
-model.fit(train_generator, epochs=10)
+model.fit(train_generator, epochs=10, batch_size=256)
 
 
 
---
+++ un.py
@@ -0,0 +1,27 @@
+import os
+
+def limiter_elements_sous_dossiers(dossier_racine, X):
+    """
+    Parcourt chaque sous-dossier du dossier racine et limite le nombre d'éléments à X.
+    Args:
+        dossier_racine (str): Chemin du dossier racine à parcourir.
+        X (int): Nombre maximal d'éléments à conserver dans chaque sous-dossier.
+    """
+    for dossier_actuel, sous_dossiers, fichiers in os.walk(dossier_racine):
+        for sous_dossier in sous_dossiers:
+            chemin_sous_dossier = os.path.join(dossier_actuel, sous_dossier)
+            elements_dans_sous_dossier = os.listdir(chemin_sous_dossier)
+            elements_a_conserver = elements_dans_sous_dossier[:X]
+
+            # Supprimer les éléments excédentaires
+            for element in elements_dans_sous_dossier:
+                chemin_element = os.path.join(chemin_sous_dossier, element)
+                if element not in elements_a_conserver:
+                    os.remove(chemin_element)
+
+            print(f"Éléments dans {sous_dossier} limités à {X} éléments.")
+
+# Exemple d'utilisation
+dossier_racine = "NULL"
+x = 20
+limiter_elements_sous_dossiers(dossier_racine, x)
